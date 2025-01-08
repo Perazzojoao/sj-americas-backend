0dcf37573d6b42a8d99ad1e5181973e7195ac93a
Perazzojoao <perazzojoao@gmail.com>
2025-01-08 20:52:44 -0300

    Adicionar validações ao DTO de criação de mesas para garantir tipos e limites adequados

--- src/modules/tables/dto/create-table.dto.ts
+++ src/modules/tables/dto/create-table.dto.ts
@@ -1,1 +1,23 @@
-export class CreateTableDto {}
+import { IsBoolean, IsIn, IsInt, IsOptional, Max, Min } from "class-validator";
+
+export class CreateTableDto {
+
+  @IsOptional()
+  @IsInt({ message: 'Owner must be a integer' })
+  @Min(1, { message: 'Owner must be greater than 0' })
+  owner?: number;
+
+  @IsOptional()
+  @IsBoolean({ message: 'is_taken must be a boolean' })
+  is_taken?: boolean;
+
+  @IsOptional()
+  @IsBoolean({ message: 'is_paid must be a boolean' })
+  is_paid?: boolean;
+
+  @IsOptional()
+  @IsInt({ message: 'Seats must be a integer' })
+  @Min(1, { message: 'Seats must be greater than 0' })
+  @Max(10, { message: 'Seats must be less than 11' })
+  seats?: number;
+}
