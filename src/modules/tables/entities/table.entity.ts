db897a2967a9b069e927b646a425b8b4455c12e1
Perazzojoao <perazzojoao@gmail.com>
2025-01-08 20:52:35 -0300

    Adicionar entidade TableEntity com propriedades e construtor para integração com o modelo de dados

--- src/modules/tables/entities/table.entity.ts
+++ src/modules/tables/entities/table.entity.ts
@@ -1,1 +1,17 @@
-export class Table {}
+import { Tables } from '@prisma/client';
+
+export class TableEntity implements Tables {
+  id: number;
+  number: number;
+  seats: number;
+  owner: number | null;
+  eventId: number;
+  isTaken: boolean;
+  isPaid: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+
+  constructor(partial: Partial<TableEntity>) {
+    Object.assign(this, partial);
+  }
+}
