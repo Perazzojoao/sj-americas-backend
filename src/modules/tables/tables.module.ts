f0907158ee026b32fba86d3d3fa355f54b436225
Perazzojoao <perazzojoao@gmail.com>
2025-01-08 21:09:00 -0300

    Adicionar repositório abstrato ao módulo de mesas para gerenciamento de dependências

--- src/modules/tables/tables.module.ts
+++ src/modules/tables/tables.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TablesService } from './tables.service';
 import { TablesController } from './tables.controller';
+import { TablesAbstractRepository } from './repositories/tables-abstract.repository';
+import { TablesRepository } from './repositories/tables.repository';
 
 @Module({
   controllers: [TablesController],
-  providers: [TablesService],
+  providers: [
+    TablesService,
+    {
+      provide: TablesAbstractRepository,
+      useClass: TablesRepository,
+    },
+  ],
 })
 export class TablesModule {}
