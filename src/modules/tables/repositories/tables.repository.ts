592e81dfbb845738dd606b5790ea77c2f29cb394
Perazzojoao <perazzojoao@gmail.com>
2025-01-08 21:08:43 -0300

    Implementar métodos de CRUD na repositório de mesas com tratamento de erros

--- src/modules/tables/repositories/tables.repository.ts
+++ src/modules/tables/repositories/tables.repository.ts
@@ -4,7 +4,7 @@ import { TablesAbstractRepository } from './tables-abstract.repository';
 import { DatabaseService } from 'src/database/database.service';
 
 export class TablesRepository implements TablesAbstractRepository {
-  constructor (private readonly prisma: DatabaseService) {}
+  constructor(private readonly prisma: DatabaseService) {}
 
   async createTable(tableEntity: TableEntity): Promise<TableEntity> {
     try {
@@ -18,21 +18,57 @@ export class TablesRepository implements TablesAbstractRepository {
   }
 
   async findAllTables(): Promise<TableEntity[]> {
-    throw new Error('Method not implemented.');
+    try {
+      return await this.prisma.tables.findMany();
+    } catch (error) {
+      console.log(error.message);
+      throw new InternalServerErrorException('Failed to fetch table list');
+    }
   }
 
   async findTableById(id: number): Promise<TableEntity> {
-    throw new Error('Method not implemented.');
+    try {
+      return await this.prisma.tables.findUnique({
+        where: {
+          id,
+        },
+      });
+    } catch (error) {
+      console.log(error.message);
+      throw new InternalServerErrorException('Failed to fetch table');
+    }
   }
 
   async updateTable(
     id: number,
     tableEntity: TableEntity,
   ): Promise<TableEntity> {
-    throw new Error('Method not implemented.');
+    try {
+      return await this.prisma.tables.update({
+        where: {
+          id,
+        },
+        data: {
+          ...tableEntity,
+          updatedAt: new Date(),
+        },
+      });
+    } catch (error) {
+      console.log(error.message);
+      throw new InternalServerErrorException('Failed to update table');
+    }
   }
 
   async deleteTable(id: number): Promise<TableEntity> {
-    throw new Error('Method not implemented.');
+    try {
+      return await this.prisma.tables.delete({
+        where: {
+          id,
+        },
+      });
+    } catch (error) {
+      console.log(error.message);
+      throw new InternalServerErrorException('Failed to delete table');
+    }
   }
 }
