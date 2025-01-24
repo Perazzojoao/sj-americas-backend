3d6c79fd8645a80ca7328275e72d965702eca692
Perazzojoao <perazzojoao@gmail.com>
2025-01-24 15:53:35 -0300

    Adicionar repositório de usuários com métodos para criar, buscar, atualizar e deletar usuários

---
+++ src/modules/users/repositories/users-abstract.repository.ts
@@ -0,0 +1,10 @@
+import { UserEntity } from '../entities/user.entity';
+
+export abstract class UsersAbstractRepository {
+  abstract createUser(userEntity: UserEntity): Promise<UserEntity>;
+  abstract findAllUsers(): Promise<UserEntity[]>;
+  abstract findUserById(id: number): Promise<UserEntity>;
+  abstract findUserByName(user_name: string): Promise<UserEntity>;
+  abstract updateUser(id: number, userEntity: UserEntity): Promise<UserEntity>;
+  abstract deleteUser(id: number): Promise<UserEntity>;
+}
---
+++ src/modules/users/repositories/users.repository.ts
@@ -0,0 +1,85 @@
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { UserEntity } from '../entities/user.entity';
+import { UsersAbstractRepository } from './users-abstract.repository';
+import { DatabaseService } from 'src/database/database.service';
+
+@Injectable()
+export class UsersRepository implements UsersAbstractRepository {
+  constructor(private readonly prisma: DatabaseService) {}
+
+  async createUser(userEntity: UserEntity): Promise<UserEntity> {
+    try {
+      return await this.prisma.user.create({
+        data: userEntity,
+      });
+    } catch (error) {
+      console.log(error.message);
+      throw new InternalServerErrorException('Failed to create user');
+    }
+  }
+
+  async findAllUsers(): Promise<UserEntity[]> {
+    try {
+      return await this.prisma.user.findMany();
+    } catch (error) {
+      console.log(error.message);
+      throw new InternalServerErrorException('Failed to fetch user list');
+    }
+  }
+
+  async findUserById(id: number): Promise<UserEntity> {
+    try {
+      return await this.prisma.user.findUnique({
+        where: {
+          id,
+        },
+      });
+    } catch (error) {
+      console.log(error.message);
+      throw new InternalServerErrorException('Failed to fetch user');
+    }
+  }
+
+  async findUserByName(user_name: string): Promise<UserEntity> {
+    try {
+      return await this.prisma.user.findUnique({
+        where: {
+          user_name,
+        },
+      });
+    } catch (error) {
+      console.log(error.message);
+      throw new InternalServerErrorException('Failed to fetch user');
+    }
+  }
+
+  async updateUser(id: number, userEntity: UserEntity): Promise<UserEntity> {
+    try {
+      return await this.prisma.user.update({
+        where: {
+          id,
+        },
+        data: userEntity,
+      });
+    } catch (error) {
+      console.log(error.message);
+      throw new InternalServerErrorException('Failed to update user');
+    }
+  }
+
+  async deleteUser(id: number): Promise<UserEntity> {
+    try {
+      return await this.prisma.user.delete({
+        where: {
+          id,
+        },
+      });
+    } catch (error) {
+      if (error.code === 'P2025') {
+        throw new NotFoundException('User not found');
+      }
+      console.log(error.message);
+      throw new InternalServerErrorException('Failed to delete user');
+    }
+  }
+}
