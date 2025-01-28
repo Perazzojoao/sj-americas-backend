3c9f229bd908a0f59b0ad0fc6d1e08f6a3fc0799
Perazzojoao <perazzojoao@gmail.com>
2025-01-28 14:47:24 -0300

    adicionar AuthGuard para gerenciamento de autenticação de rotas

---
+++ src/resources/guards/auth.guard.ts
@@ -0,0 +1,62 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtPayload, JwtTokenService } from 'src/jwt/jwt-token.service';
+import { Request } from 'express';
+
+export interface RequestWithUser extends Request {
+  user: JwtPayload;
+}
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly jwtTokenService: JwtTokenService,
+    private reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const ignore = this.reflector.get<boolean>(
+      'public-route',
+      context.getHandler(),
+    );
+    if (ignore) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
+
+    const token = this.getToken(request);
+    if (!token) {
+      throw new UnauthorizedException('Authentication error');
+    }
+
+    const payload = await this.validateToken(token);
+    request.user = payload;
+
+    return true;
+  }
+
+  private getToken(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    if (type !== 'Bearer') {
+      return;
+    }
+
+    return token;
+  }
+
+  private async validateToken(token: string): Promise<JwtPayload> {
+    try {
+      const payload = await this.jwtTokenService.verifyToken(token);
+      return payload;
+    } catch (error) {
+      console.error(error);
+      throw new UnauthorizedException('Token inválido');
+    }
+  }
+}
