c481f9c8b4f1ab08f2d4b2a7bd1325f57a625172
Perazzojoao <perazzojoao@gmail.com>
2025-01-30 13:27:26 -0300

    adicionar lógica para criar um usuário administrador com senha hash ao inicializar o módulo

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,9 +1,20 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
+import { hashPassword } from 'src/lib/hash-password';
 
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit {
+  constructor(private readonly configService: ConfigService) {
+    super();
+  }
+
   async onModuleInit() {
     await this.$connect();
+    const adminPassword = this.configService.get<string>('ADMIN_SECRET_KEY');
+    const hashedPassword = await hashPassword(adminPassword);
+    const time = new Date();
+    await this
+      .$executeRaw`INSERT INTO users (user_name, password, role, createdAt, updatedAt) VALUES ('admin',${hashedPassword}, 'ADMIN', ${time}, ${time}) ON CONFLICT (user_name) DO NOTHING`;
   }
 }
