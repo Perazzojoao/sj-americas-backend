2f853a269f86bc678676059043ce36d23ee0f143
Perazzojoao <perazzojoao@gmail.com>
2025-02-05 22:54:14 -0300

    adicionar campo "created_by" na entidade de usuário e criar a entidade "CreatedBy" para relacionamentos

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,14 +1,23 @@
-import { $Enums, User } from '@prisma/client';
+import { $Enums, CreatedBy, User } from '@prisma/client';
 
 export class UserEntity implements User {
   id: number;
   user_name: string;
   password: string;
   role: $Enums.Role;
+  created_by: number | null;
+  createdBy?: CreatedByEntity;
   createdAt: Date;
   updatedAt: Date;
 
   constructor(partial: Partial<UserEntity>) {
     Object.assign(this, partial);
   }
+}
+
+export class CreatedByEntity implements CreatedBy{
+  id: number;
+  user_name: string;
+  user_id: number;
+  created_users?: UserEntity[];
 }
